9fc4d782239996c114b181561df0b09ff44f329a
kentaro <kentaro.yoneda.2000@gmail.com>
2026-05-16 00:09:22 +0900

    20260515(review font-size)

--- backend/main.py
+++ backend/main.py
@@ -52,9 +52,6 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SESSION_COOKIE_SAMESITE"] = "None"
 app.config["SESSION_COOKIE_SECURE"] = True
 
-print("SECRET_KEY:", app.secret_key)
-print("FRONTEND_ORIGINS:", allowed_origins)
-
 db = SQLAlchemy()
 migrate = Migrate()
 
