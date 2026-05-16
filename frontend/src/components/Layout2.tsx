2cd37cfa957b7bccc1ed9379eed6e5d716253bd9
kentaro <kentaro.yoneda.2000@gmail.com>
2026-05-17 01:01:07 +0900

    20260516(resize title-icon 2)

--- frontend/src/components/Layout2.tsx
+++ frontend/src/components/Layout2.tsx
@@ -2,14 +2,16 @@ import '../reset.css';
 import { Outlet } from "react-router-dom";
 import Message from './Message.tsx';
 import TitleIcon from '../img/TitleIcon.svg';
+import TitleIconForMb from '../img/TitleIcon_for_mb.svg';
 
-function Layout() {
+function Layout2() {
 
     return (
         <div>
             <header>
                 <h1>
                     <img src={TitleIcon} alt="Title Icon" className='title-icon'/>
+                    <img src={TitleIconForMb} alt="Title Icon for Mobile" className='title-icon-for-mb' />
                 </h1>
             </header>
             <Outlet />
@@ -18,4 +20,4 @@ function Layout() {
     );
 }
 
-export default Layout;
+export default Layout2;
